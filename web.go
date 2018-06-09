cad840867549cc3438e885cb4d847d7806ef3abd
mohamedveron <mohamedveron23@gmail.com>
2018-06-09 17:45:35 +0200

    cors problem resolved

--- web.go
+++ web.go
@@ -31,7 +31,14 @@ func dbConn() (db *sql.DB) {
     return db
 }
 
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+    (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
 func login(w http.ResponseWriter, r *http.Request) {
+    setupResponse(&w, r)
    var person Person
     _ = json.NewDecoder(r.Body).Decode(&person)
      if checkUser(person){
@@ -61,6 +68,7 @@ func Insert(person Person) {
 
 func register(w http.ResponseWriter, r *http.Request) {
     //params := mux.Vars(r)
+    setupResponse(&w, r)
     var person Person
     _ = json.NewDecoder(r.Body).Decode(&person)
      Insert(person)
